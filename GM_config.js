072752de7a85cd68dc7b200286908c035cb42977
eight <eight04@gmail.com>
2015-04-16 22:55:32 +0800

    Bump version number

--- GM_config.js
+++ GM_config.js
@@ -3,7 +3,7 @@
 // @description	A library to help you set up configure in greasemonkey script.
 // @namespace   eight04.blogspot.com
 // @include     http*
-// @version     0.3.0
+// @version     0.3.1
 // @grant       GM_setValue
 // @grant		GM_getValue
 // @license		MIT
